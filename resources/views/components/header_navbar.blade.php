6377bb95236e5e3952993c146b75d754464a233d
rapilap <raffyabdillah03@gmail.com>
2026-09-26 10:35:23 +0700

    add: nav and wa button on navbar

--- resources/views/components/header_navbar.blade.php
+++ resources/views/components/header_navbar.blade.php
@@ -1,7 +1,8 @@
 {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
-<div class="bg-white text-[#1b1b18] dark:text-black flex items-center lg:justify-center h-30.5 flex-col drop-shadow-md">
-    <div class="bg-[#000080] text-white font-normal text-[13px] flex flex-row items-center justify-between py-2 px-8 h-fit w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+<div class="bg-white text-[#1b1b18] dark:text-black flex items-center lg:justify-center h-26 flex-col drop-shadow-md">
+    {{-- Top --}}
+    <div class="bg-b1 text-white font-normal text-[13px] flex flex-row items-center justify-between py-2 px-8 h-fit w-full transition-opacity opacity-100 duration-750 starting:opacity-0">
         <div class="flex items-start justify-center gap-4.5">
             <div class="flex items-center justify-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024">
@@ -40,7 +41,65 @@
         </div>
     </div>
 
-    <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-        halo, aku navbar
+    {{-- Bottom --}}
+    <div class="flex items-center justify-between w-full transition-opacity opacity-100 duration-750 lg:grow py-2 px-8 starting:opacity-0">
+        {{-- Logo --}}
+        <a href="{{ route('beranda') }}" class="flex flex-row items-center justify-center gap-2">
+            <div class="flex items-center justify-center gap-5 bg-b1 text-white p-2 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024">
+                    <path d="M0 0h1024v1024H0z" fill="none" />
+                    <path fill="currentColor" d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2M568 868H456V664h112zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7l23.1 23.1L882 542.3z" />
+                </svg>
+            </div>
+                <h1 class="font-bold font-fraunces text-[20px]">    
+                Pulihkan
+            </h1>
+        </a>
+
+        {{-- Navigation --}}
+        <div class="flex items-center justify-center gap-5 opacity-100 transition-opacity duration-750 starting:opacity-0">
+            <a href="{{ route('beranda') }}" class="{{ request()->routeIs('beranda')
+            ? 'text-b1 border-b-2 border-g1'
+            : 'text-b4 hover:text-b1' }}"
+            font-normal text-[13px] transition-colors duration-500>
+                Beranda
+            </a>
+
+            <a href="{{ route('event') }}" class="{{ request()->routeIs('event')
+            ? 'text-b1 border-b-2 border-g1'
+            : 'text-b4 hover:text-b1' }}"
+            font-normal text-[13px] transition-colors duration-500>
+                Event
+            </a>
+
+            <a href="{{ route('layanan') }}" class="{{ request()->routeIs('layanan')
+            ? 'text-b1 border-b-2 border-g1'
+            : 'text-b4 hover:text-b1' }}"
+            font-normal text-[13px] transition-colors duration-500>
+                Layanan
+            </a>
+
+            <a href="{{ route('testimoni') }}" class="{{ request()->routeIs('testimoni')
+            ? 'text-b1 border-b-2 border-g1'
+            : 'text-b4 hover:text-b1' }}"
+            font-normal text-[13px] transition-colors duration-500>
+                Testimoni
+            </a>
+        </div>
+
+        {{-- WA Button --}}
+        <div>
+            <a href="https://wa.me/62xxxxxxxxxxx?text=halo, chat ini dikirim melalui tombol pada website!%0ANama: [Nama]" target="_blank" class="inline-flex items-center gap-2 rounded-3xl bg-b1 px-4 py-2 text-white transition hover:opacity-90">
+                <span class="font-normal text-[13px]">Booking Via</span>
+                <div class="text-[#25D366] flex items-center justify-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path fill="currentColor" d="M8.886 7.17c.183.005.386.015.579.443c.128.285.343.81.519 1.238c.137.333.249.607.277.663c.064.128.104.275.02.448l-.028.058a1.4 1.4 0 0 1-.23.37l-.143.17c-.085.104-.17.206-.242.278c-.129.128-.262.266-.114.522s.668 1.098 1.435 1.777a6.6 6.6 0 0 0 1.903 1.2q.105.045.17.076c.257.128.41.108.558-.064c.149-.173.643-.749.817-1.005c.168-.256.34-.216.578-.128c.238.089 1.504.71 1.761.837l.143.07c.179.085.3.144.352.23c.064.109.064.62-.148 1.222c-.218.6-1.267 1.176-1.742 1.22l-.135.016c-.436.052-.988.12-2.956-.655c-2.426-.954-4.027-3.32-4.35-3.799l-.053-.076l-.006-.008c-.147-.197-1.048-1.402-1.048-2.646c0-1.19.587-1.81.854-2.092l.047-.05a.95.95 0 0 1 .687-.32c.173 0 .347 0 .495.005" />
+                        <path fill="currentColor" fill-rule="evenodd" d="M2.184 21.331a.4.4 0 0 0 .487.494l4.607-1.204a10 10 0 0 0 4.76 1.207h.004c5.486 0 9.958-4.446 9.958-9.912a9.83 9.83 0 0 0-2.914-7.011A9.92 9.92 0 0 0 12.042 2c-5.486 0-9.958 4.446-9.958 9.911c0 1.739.458 3.447 1.33 4.954zm2.677-4.068a1.5 1.5 0 0 0-.148-1.15a8.4 8.4 0 0 1-1.129-4.202c0-4.63 3.793-8.411 8.458-8.411c2.27 0 4.388.877 5.986 2.468a8.33 8.33 0 0 1 2.472 5.948c0 4.63-3.793 8.412-8.458 8.412h-.005a8.5 8.5 0 0 1-4.044-1.026a1.5 1.5 0 0 0-1.094-.132l-2.762.721z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="font-normal text-[13px]">WhatsApp</span>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,16 @@ Route::get('/', function () {
 
 Route::get('/beranda', function () {
     return view('main');
-});
+})->name('beranda');
+
+Route::get('/event', function () {
+    return view('event');
+})->name('event');
+
+Route::get('/layanan', function () {
+    return view('layanan');
+})->name('layanan');
+
+Route::get('/testimoni', function () {
+    return view('testimoni');
+})->name('testimoni');
